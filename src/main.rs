9dacdced5672f35e32d7f52c2a9d2f748b582921
Puppet <doggyandkira2013@gmail.com>
2024-10-09 19:27:35 -0700

    Added -h flag
    -h makes it so instead of printing a, b, etc. It prints the hex versions
    of those.
    
    PsychedelicShayna (On github) made the code for printing the hex values

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,43 @@
 use std::io::{self, Read};
+use std::env;
 
 fn main() {
-    let mut input = String::new();
+    let args: Vec<String> = env::args().collect();
+
+    if args.contains(&String::from("-h")) {
+        let mut input = Vec::<u8>::new();
+        io::stdin().read_to_end(&mut input).expect("Failed to read from stdin");
     
-    // Read from stdin (basically gets what was piped)
-    io::stdin().read_to_string(&mut input).expect("Failed to read from stdin");
-
-    let mut char_count = std::collections::HashMap::new();
-
-    for c in input.chars() {
-        let key = match c {
-            ' ' => "space".to_string(),
-            '\n' => "newline".to_string(),
-            _ => c.to_string(),
-        };
-        *char_count.entry(key).or_insert(0) += 1;
-    }
-
-    for (c, count) in &char_count {
-        println!("{}: {}", c, count);
+        let mut byte_count = [0; 256];
+    
+        for byte in input {
+            byte_count[byte as usize] += 1;
+        }
+    
+        for (byte, count) in byte_count.iter().enumerate() {
+            if *count > 0 {
+                println!("{:02x}: {}", byte, count);
+            }
+        }
+    } else {
+        let mut input = String::new();
+    
+        // Read from stdin (basically gets what was piped)
+        io::stdin().read_to_string(&mut input).expect("Failed to read from stdin");
+    
+        let mut char_count = std::collections::HashMap::new();
+    
+        for c in input.chars() {
+            let key = match c {
+                ' ' => "space".to_string(),
+                '\n' => "newline".to_string(),
+                _ => c.to_string(),
+            };
+            *char_count.entry(key).or_insert(0) += 1;
+        }
+    
+        for (c, count) in &char_count {
+            println!("{}: {}", c, count);
+        }    
     }
 }
